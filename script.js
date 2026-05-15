f803a1b43bcbf8128029e308940fb09d5083e8f6
Avi Aaron Batchu <aviaaron2003@gmail.com>
2026-05-15 14:11:25 -0500

    added in current weather functionality

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const cityInput = document.getElementById('city-input');
-const searchButton = document.getElementById('get-weather');
+const form = document.getElementById('weather-form');
+const statusElement = document.getElementById('status-container');
+
+const weatherContainer = document.getElementById('weather-result');
 const temperatureElement = document.getElementById('temperature');
 const descriptionElement = document.getElementById('description');
 const humidityElement = document.getElementById('humidity');
@@ -7,3 +10,35 @@ const cityNameElement = document.getElementById('city-name');
 const windSpeedElement = document.getElementById('wind-speed');
 
 const apikey = "15f0d3f8be73eb957be7bd699d022eed";
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const city = cityInput.value.trim();
+    if (city === '') {
+        alert('Please enter a city name.');
+        return;
+    }
+
+    statusElement.classList.add("hidden");
+    weatherContainer.classList.add("hidden");
+    
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        cityNameElement.textContent = data.name;
+        temperatureElement.textContent = 'Temperature: ' + data.main.temp + ' °C';
+        descriptionElement.textContent = 'Description: ' + data.weather[0].description;
+        humidityElement.textContent = 'Humidity: ' + data.main.humidity + '%';
+        windSpeedElement.textContent = 'Wind Speed: ' + data.wind.speed + ' m/s';
+
+        weatherContainer.classList.remove("hidden");
+
+    } catch (error) {
+        statusElement.textContent = error.message;
+        statusElement.classList.remove("hidden");
+    }
+});
