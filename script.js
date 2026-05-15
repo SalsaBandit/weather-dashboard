2d677f73ca8d2f7cc7ac82ded011e7f37d6a499b
Avi Aaron Batchu <aviaaron2003@gmail.com>
2026-05-15 04:13:01 -0500

    added containers and other core files

---
+++ script.js
@@ -0,0 +1,9 @@
+const cityInput = document.getElementById('city-input');
+const searchButton = document.getElementById('get-weather');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+const cityNameElement = document.getElementById('city-name');
+const windSpeedElement = document.getElementById('wind-speed');
+
+const apikey = "15f0d3f8be73eb957be7bd699d022eed";
